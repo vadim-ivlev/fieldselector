6b12b595abbf2d184fe1736f128164e269d3a5c8
vadim-ivlev <vadim.ivlev@gmail.com>
2021-02-18 07:01:38 +0300

    README and comments

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # fieldselector
-List fields returned by a GraphQL function
+
+Getting list of fields that must be returned by a GraphQL function inside a resolver.
+
+Can be used with most golang GraphQL frameworks.
+
+Sometimes while writing a GraphQL resolver in golang,
+we want to know a list of fields required to to be returned to the user.
+Knowing the names of fields allows to request databases 
+in more accurate and efficient way.
+
+
+
--- fieldselector.go
+++ fieldselector.go
@@ -1,8 +1,7 @@
-// Примитивный парсер GraphQL запроса.
-// Экспортируемая функция GetSelectedFields
-// возвращает список полей в тексте запроса GraphQL
-// выбранных для возврата из функции внутри фигурных скобок.
-
+// A primitive GraphQL query parser.
+// Function GetSelectedFields
+// returns a list of fields inside curly braces
+// selected to be returned from a GraphQL function.
 package fieldselector
 
 import (
@@ -10,12 +9,12 @@ import (
 	"strings"
 )
 
-// идущие подряд пробельные символы
+// consecutive whitespace characters
 var regSpaces, _ = regexp.Compile(`\s+`)
 
-// Находит скобку и возвращает ее позицию и позицию парной ей скобки.
-// - brackets - Какие скобки искать. Например "()" или "{}"
-// - s - строка где нужно найти скобки
+// Returns positions of a parenthesis and of its paired parenthesis.
+// - brackets - What brackets to look for.For example "()" or "{}"
+// - s - string to search for brackets
 func findBrackets(brackets, s string) (ind0, ind1 int) {
 	ind0, ind1 = -1, -1
 	if len(brackets) < 2 {
@@ -42,8 +41,8 @@ func findBrackets(brackets, s string) (ind0, ind1 int) {
 	return
 }
 
-// Возвращает текст внутри скобок
-// - s - строка где нужно найти скобки
+// Returns the text inside the brackets
+// - s - string to search for brackets
 func insideBrackets(brackets, s string) string {
 	var i0, i1 = findBrackets(brackets, s)
 	if i0 == -1 || i1 == -1 {
@@ -52,8 +51,8 @@ func insideBrackets(brackets, s string) string {
 	return s[i0+1 : i1]
 }
 
-// Возвращает текст снаружи скобок (вырезает то что внутри скобок).
-// - s - строка где нужно найти скобки
+// Returns the text outside the brackets (cuts out the inner part).
+// - s - string to search for brackets
 func outsideBrackets(brackets, s string) string {
 	var i0, i1 = findBrackets(brackets, s)
 	if i0 == -1 || i1 == -1 {
@@ -62,8 +61,8 @@ func outsideBrackets(brackets, s string) string {
 	return s[:i0] + s[i1+1:]
 }
 
-// возвращает текст внутри фигурных скобок найденных после подстроки
-// удовлетворяющей регулярному выражению
+// returns the text inside the curly braces located after a substring
+// that matches the regular expression
 func selectionAfter(regexpStr, s string) string {
 	var re, _ = regexp.Compile(regexpStr)
 	var loc = re.FindStringIndex(s)
@@ -74,17 +73,17 @@ func selectionAfter(regexpStr, s string) string {
 	return insideBrackets("{}", ss)
 }
 
-// удаляет ведущие, лидирующие и лишние пробелы внутри строки
+// removes leading, trailing and extra spaces within the string
 func removeSpaces(s string) string {
 	var ss = regSpaces.ReplaceAllString(s, " ")
 	return strings.Trim(ss, " ")
 }
 
-// GetSelectedFields возвращает список полей в тексте запроса GraphQL
-// выбранных для возврата из функции внутри фигурных скобок.
-// - selectionPath - путь, включая имя функции, для которого возвращается список.
-// может быть неполным или с пропусками.
-// - query - текст GraphQL запроса
+// GetSelectedFields returns a list of fields in the GraphQL query
+// selected to return from a function inside curly braces.
+// - selectionPath - path, including a function name, for which the list is returned.
+// May be incomplete or missing.
+// - query - GraphQL query text
 func GetSelectedFields(selectionPath []string, query string) []string {
 	// (?s) - multiline search, .+? not greedy search
 	var regexpStr = `(?s)` + strings.Join(selectionPath, `.+?`)
